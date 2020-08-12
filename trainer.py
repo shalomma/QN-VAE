49a97337b9f28f782a0f16fe872ae231697da424
Maayan Shalom <sha.maayan@gmail.com>
2020-08-12 23:06:32 +0300

    clean trainer

--- trainer.py
+++ trainer.py
@@ -21,25 +21,19 @@ class Trainer:
             to_print = f'Batch {i:04}: '
             for phase in self.phases:
                 self.model.train() if phase == 'train' else self.model.eval()
-                # epoch_recon_error = 0
-                # epoch_perplexity = 0
-                # for k, (samples, _) in enumerate(self.loader[phase]):
                 samples, _ = next(iter(self.loader[phase]))
                 samples = samples.to(self.device)
                 self.optimizer.zero_grad()
                 vq_loss, data_recon, perplexity = self.model(samples)
                 recon_error = F.mse_loss(data_recon, samples) / self.data_variance
-                # epoch_recon_error += recon_error.item()
+                self.train_recon_error.append(recon_error.item())
                 loss = recon_error
                 if isinstance(self.model, VQ_VAE):
                     loss += vq_loss
-                    # epoch_perplexity += perplexity.item()
                 if phase == 'train':
                     loss.backward()
                     self.optimizer.step()
 
-                # n_elements = np.prod(self.loader[phase].dataset.data.shape)
-                self.train_recon_error.append(recon_error.item())
                 to_print += f'{phase}: '
                 to_print += f'recon error: {np.mean(self.train_recon_error[-100:]):.4f}  '
                 if isinstance(self.model, VQ_VAE):
