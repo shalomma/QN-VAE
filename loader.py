33cfa9524ab9e85341864abb24e6bf871df9da06
Maayan Shalom <sha.maayan@gmail.com>
2020-08-17 20:46:21 +0300

    ImageNetLoader compose

--- loader.py
+++ loader.py
@@ -10,8 +10,6 @@ class Loader:
     def __init__(self):
         manual_seed(self.seed)
         self.data = dict()
-        self.compose = transforms.Compose([transforms.ToTensor(),
-                                           transforms.Normalize((0.5, 0.5, 0.5), (1.0, 1.0, 1.0))])
 
     def get(self, batch_size):
         loaders = dict()
@@ -23,12 +21,30 @@ class Loader:
 class CIFAR10Loader(Loader):
     def __init__(self):
         super(CIFAR10Loader, self).__init__()
-        self.data['train'] = datasets.CIFAR10(root="data", train=True, download=True, transform=self.compose)
-        self.data['val'] = datasets.CIFAR10(root="data", train=False, download=True, transform=self.compose)
+        compose = transforms.Compose([transforms.ToTensor(),
+                                      transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                                           std=(1.0, 1.0, 1.0))
+                                      ])
+        self.data['train'] = datasets.CIFAR10(root="data", train=True, download=True, transform=compose)
+        self.data['val'] = datasets.CIFAR10(root="data", train=False, download=True, transform=compose)
 
 
 class ImageNetLoader(Loader):
     def __init__(self):
         super(ImageNetLoader, self).__init__()
-        self.data['train'] = datasets.ImageNet(root="data", split='train', transform=self.compose)
-        self.data['val'] = datasets.ImageNet(root="data", split='val', transform=self.compose)
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        compose_train = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+            ])
+        compose_val = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize
+        ])
+        self.data['val'] = datasets.ImageNet(root="data", split='val', transform=compose_val)
+        self.data['train'] = datasets.ImageNet(root="data", split='train', transform=compose_train)
