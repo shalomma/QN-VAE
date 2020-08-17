a28b2ddde1f123cfa8296219b5403fec74538105
Maayan Shalom <sha.maayan@gmail.com>
2020-08-17 21:55:59 +0300

    params

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ torch.manual_seed(seed)
 
 
 if __name__ == '__main__':
-    batch_size = 128
-    epochs = 2000
-    num_hidden = 128
-    num_residual_hidden = 32
-    num_residual_layers = 2
-    embedding_dim = 64
-    num_embeddings = 512
-    commitment_cost = 0.25
-    learning_rate = 1e-4
+    params = {
+        'batch_size': 128,
+        'epochs': 2000,
+        'num_hidden': 128,
+        'num_residual_hidden': 32,
+        'num_residual_layers': 2,
+        'embedding_dim': 64,
+        'num_embeddings': 512,
+        'commitment_cost': 0.25,
+        'learning_rate': 1e-3
+    }
 
     quant_noise_probs = [0, 0.25, 0.5, 0.75, 1]
 
@@ -28,19 +30,19 @@ if __name__ == '__main__':
     qn_model = dict()
     optimizer = dict()
     for q in quant_noise_probs:
-        qn_model[q] = VQ_VAE(num_hidden, num_residual_layers, num_residual_hidden,
-                             num_embeddings, embedding_dim, commitment_cost).to(device)
-    qn_model[0] = AE(num_hidden, num_residual_layers,
-                     num_residual_hidden, embedding_dim).to(device)
+        qn_model[q] = VQ_VAE(params['num_hidden'], params['num_residual_layers'], params['num_residual_hidden'],
+                             params['num_embeddings'], params['embedding_dim'], params['commitment_cost']).to(device)
+    qn_model[0] = AE(params['num_hidden'], params['num_residual_layers'],
+                     params['num_residual_hidden'], params['embedding_dim']).to(device)
     for q in quant_noise_probs:
-        optimizer[q] = Adam(qn_model[q].parameters(), lr=learning_rate, amsgrad=False)
+        optimizer[q] = Adam(qn_model[q].parameters(), lr=params['learning_rate'], amsgrad=False)
 
-    loaders = loader.CIFAR10Loader().get(batch_size)
+    loaders = loader.CIFAR10Loader().get(params['batch_size'])
 
     for q in quant_noise_probs:
         print(f'Train q={q}')
         trainer = Trainer(qn_model[q], optimizer[q], loaders)
-        trainer.epochs = epochs
+        trainer.epochs = params['epochs']
         trainer.run()
         torch.save(qn_model[q].state_dict(), f'model_{q}.pt')
         with open(f'model_{q}.pkl', 'wb') as f:
