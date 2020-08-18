75480a10348676b39d53aa718f30fb592413f5f3
Maayan Shalom <sha.maayan@gmail.com>
2020-08-18 19:47:26 +0300

    encoding.py

---
+++ encoding.py
@@ -0,0 +1,37 @@
+import argparse
+import pickle
+import torch
+
+import loader
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('timestamp', type=str, help='models timestamp')
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    batch_size = 1024
+    quant_noise_probs = [0, 0.25, 0.5, 0.75, 1]
+    qn_model = dict()
+    for q_ in quant_noise_probs:
+        with open(f'models/{args.timestamp}/model_{q_}_cpu.pkl', 'rb') as f:
+            qn_model[q_] = pickle.load(f).to(device)
+
+    loaders = loader.CIFAR10Loader().get(batch_size)
+    for q_, model in qn_model.items():
+        if q_ == 0:
+            continue
+        print(f'Encoding using {q_} QN-VAE')
+        model.eval()
+        encode_dataset = torch.tensor([]).to(device)
+        labels_dataset = torch.tensor([]).to(device)
+        for phase in ['train', 'val']:
+            for samples, labels in loaders[phase]:
+                samples, labels = samples.to(device), labels.to(device)
+                _, _, _, encoding = model(samples)
+                encode_dataset = torch.cat((encode_dataset, encoding))
+                labels_dataset = torch.cat((labels_dataset, labels))
+        torch.save(encode_dataset, f'./data/encoded_{q_}.pt')
+        torch.save(labels_dataset, f'./data/encoded_labels_{q_}.pt')
