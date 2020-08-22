7c1d12d00d02340833957abe5eb70843d9275ad6
Maayan Shalom <sha.maayan@gmail.com>
2020-08-22 19:32:22 +0300

    VAETrainer

--- trainer.py
+++ trainer.py
@@ -1,4 +1,6 @@
 import logging
+from abc import ABC, abstractmethod
+
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -10,12 +12,11 @@ cudnn.benchmark = False
 cudnn.fastest = True
 
 
-class Trainer:
+class Trainer(ABC):
     def __init__(self, model, optimizer, loader):
         self.model = model
         self.optimizer = optimizer
         self.loader = loader
-        self.data_variance = np.var(loader['train'].dataset.data / 255.0)
         self.batches = 50
         self.phases = ['train', 'val']
         self.train_recon_error = []
@@ -28,23 +29,49 @@ class Trainer:
             to_print = f'Batch {i:04}: '
             for phase in self.phases:
                 self.model.train() if phase == 'train' else self.model.eval()
-                samples, _ = next(iter(self.loader[phase]))
+                samples, labels = next(iter(self.loader[phase]))
                 samples = samples.to(self.device)
+                labels = labels.to(self.device)
                 self.optimizer.zero_grad()
-                vq_loss, data_recon, perplexity, encoding = self.model(samples)
-                recon_error = F.mse_loss(data_recon, samples) / self.data_variance
-                self.train_recon_error.append(recon_error.item())
-                loss = recon_error
-                if vq_loss is not None:
-                    loss += vq_loss
+                loss = self.compare(samples, labels)
+
                 if phase == 'train':
                     loss.backward()
                     self.optimizer.step()
 
                 to_print += f'{phase}: '
                 to_print += f'recon error: {np.mean(self.train_recon_error[-100:]):.4f}  '
-                if perplexity is not None:
-                    self.train_perplexity.append(perplexity.item())
-                    to_print += f'perplexity: {np.mean(self.train_perplexity[-100:]):.4f} '
+                # if perplexity is not None:
+                #     self.train_perplexity.append(perplexity.item())
+                #     to_print += f'perplexity: {np.mean(self.train_perplexity[-100:]):.4f} '
             if i % 100 == 0:
                 self.log(to_print)
+
+    @abstractmethod
+    def compare(self, samples, labels):
+        pass
+
+
+class VAETrainer(Trainer):
+    def __init__(self, model, optimizer, loader):
+        super(VAETrainer, self).__init__(model, optimizer, loader)
+        self.data_variance = np.var(loader['train'].dataset.data / 255.0)
+
+    def compare(self, samples, labels):
+        vq_loss, data_recon, perplexity, encoding = self.model(samples)
+        recon_error = F.mse_loss(data_recon, samples) / self.data_variance
+        self.train_recon_error.append(recon_error.item())
+        loss = recon_error
+        if vq_loss is not None:
+            loss += vq_loss
+        return loss
+
+
+class PriorTrainer(Trainer):
+    def __init__(self, model, optimizer, loader):
+        super(PriorTrainer, self).__init__(model, optimizer, loader)
+
+    def compare(self, samples, labels):
+        samples = samples.float() / (15 - 1)
+        outputs = self.model(samples, labels)
+        return F.cross_entropy(outputs, samples)
