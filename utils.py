8dafe1a21e0fbc499914fad692f0b75a20a83c55
Maayan Shalom <sha.maayan@gmail.com>
2020-08-23 23:36:08 +0300

    prefix

--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ import pickle
 import torch
 
 
-def save_model(model, params, q, directory):
-    with open(f'{directory}/model_{q}.pkl', 'wb') as f:
+def save_model(model, params, prefix, q, directory):
+    with open(f'{directory}/{prefix}_{q}.pkl', 'wb') as f:
         pickle.dump(model, f)
-    with open(f'{directory}/model_{q}_cpu.pkl', 'wb') as f:
+    with open(f'{directory}/{prefix}_{q}_cpu.pkl', 'wb') as f:
         pickle.dump(model.cpu(), f)
     with open(f'{directory}/params_{q}.pkl', 'wb') as f:
         pickle.dump(params, f)
-    torch.save(model.state_dict(), f'{directory}/model_{q}_state.pt')
-    torch.save(model, f'{directory}/model_{q}.pt')
+    torch.save(model.state_dict(), f'{directory}/{prefix}_{q}_state.pt')
+    torch.save(model, f'{directory}/{prefix}_{q}.pt')
