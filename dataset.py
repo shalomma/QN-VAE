54e4bfabfd0ce949c3ce8fc4cc6d3b705c3d0ed9
Maayan Shalom <sha.maayan@gmail.com>
2020-08-24 00:31:47 +0300

    transform(self.data).to(device)

--- dataset.py
+++ dataset.py
@@ -6,11 +6,12 @@ import torch.utils.data as data
 class Encoded(data.Dataset):
     def __init__(self, root_dir, q, indices, transform=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.data = torch.load(os.path.join(root_dir, f'encoded_{q}.pt'), map_location=device).view(-1, 8, 8, 1)
+        self.data = torch.load(os.path.join(root_dir, f'encoded_{q}.pt'), map_location=device)
+        self.data = self.data.view(-1, 8, 8, 1).cpu().numpy()
         self.y = torch.load(os.path.join(root_dir, f'encoded_labels_{q}.pt'), map_location=device)
-        self.data = self.data[indices].to(device)
         self.y = self.y[indices].to(device).long()
-        self.transform = transform
+        self.data = self.data[indices]
+        self.data = transform(self.data).to(device)
 
     def __len__(self):
         return len(self.data)
@@ -18,7 +19,4 @@ class Encoded(data.Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        z = self.data[idx]
-        if self.transform is not None:
-            z = self.transform(self.data[idx])
-        return z, self.y[idx]
+        return self.data[idx], self.y[idx]
