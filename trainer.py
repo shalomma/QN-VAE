405220b078054e675dc5f816097f89e67fbf0c0c
Maayan Shalom <sha.maayan@gmail.com>
2020-08-23 23:17:02 +0300

    PriorTrainer compare

--- trainer.py
+++ trainer.py
@@ -19,19 +19,18 @@ class Trainer(ABC):
         self.loader = loader
         self.batches = 50
         self.phases = ['train', 'val']
-        self.train_recon_error = []
-        self.train_perplexity = []
+        self.metrics = dict()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.log = logging.getLogger(__name__).info
 
     def run(self):
         for i in range(self.batches):
-            to_print = f'Batch {i:04}: '
+            to_print = f'Batch {i:04}:'
             for phase in self.phases:
                 self.model.train() if phase == 'train' else self.model.eval()
                 samples, labels = next(iter(self.loader[phase]))
-                samples = samples.to(self.device)
-                labels = labels.to(self.device)
+                samples = samples.to(self.device, non_blocking=True)
+                labels = labels.to(self.device, non_blocking=True)
                 self.optimizer.zero_grad()
                 loss = self.compare(samples, labels)
 
@@ -39,11 +38,9 @@ class Trainer(ABC):
                     loss.backward()
                     self.optimizer.step()
 
-                to_print += f'{phase}: '
-                to_print += f'recon error: {np.mean(self.train_recon_error[-100:]):.4f}  '
-                # if perplexity is not None:
-                #     self.train_perplexity.append(perplexity.item())
-                #     to_print += f'perplexity: {np.mean(self.train_perplexity[-100:]):.4f} '
+                to_print += f'\t{phase}: '
+                for metric, values in self.metrics.items():
+                    to_print += f'{metric}: {np.mean(values[-100:]):.4f}  '
             if i % 100 == 0:
                 self.log(to_print)
 
@@ -56,22 +53,34 @@ class VAETrainer(Trainer):
     def __init__(self, model, optimizer, loader):
         super(VAETrainer, self).__init__(model, optimizer, loader)
         self.data_variance = np.var(loader['train'].dataset.data / 255.0)
+        self.metrics = {
+            'loss': [],
+            'perplexity': []
+        }
 
     def compare(self, samples, labels):
         vq_loss, data_recon, perplexity, encoding = self.model(samples)
         recon_error = F.mse_loss(data_recon, samples) / self.data_variance
-        self.train_recon_error.append(recon_error.item())
+        self.metrics['loss'].append(recon_error.item())
         loss = recon_error
         if vq_loss is not None:
             loss += vq_loss
+        if perplexity is not None:
+            self.metrics['perplexity'].append(perplexity.item())
         return loss
 
 
 class PriorTrainer(Trainer):
     def __init__(self, model, optimizer, loader):
         super(PriorTrainer, self).__init__(model, optimizer, loader)
+        self.metrics = {
+            'loss': []
+        }
+        self.levels = None
 
     def compare(self, samples, labels):
-        samples = samples.float() / (15 - 1)
-        outputs = self.model(samples, labels)
-        return F.cross_entropy(outputs, samples)
+        normalized_samples = samples.float() / (self.levels - 1)
+        outputs = self.model(normalized_samples, labels)
+        loss = F.cross_entropy(outputs, samples)
+        self.metrics['loss'].append(loss.item())
+        return loss
