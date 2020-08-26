e0c8be22dcc731c0345e1f2d07a9a76002091c71
Maayan Shalom <sha.maayan@gmail.com>
2020-08-26 22:58:09 +0300

    inspect

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pickle
 import torch
+import inspect
 
 
 def save_model(model, params, prefix, q, directory):
@@ -17,9 +18,11 @@ def load_model(model_class, prefix, q, directory):
     with open(f'{directory}/{prefix}_params_{q}.pkl', 'rb') as f:
         params = pickle.load(f)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = model_class(params['num_hidden'], params['num_residual_layers'], params['num_residual_hidden'],
-                        params['num_embeddings'], params['embedding_dim'], params['commitment_cost'],
-                        quant_noise=q).to(device)
+    args = list(inspect.signature(model_class.__init__).parameters.keys())
+    params_class = dict()
+    for a in args[1:]:
+        params_class[a] = params[a]
+    model = model_class(**params_class).to(device)
     model.load_state_dict(torch.load(f'{directory}/{prefix}_{q}_state.pt'))
     model.eval()
     return model
