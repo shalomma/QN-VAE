b81b74ac66eb8a671f52b0fd793a5b8e1baf0bd2
Maayan Shalom <sha.maayan@gmail.com>
2020-08-26 23:08:01 +0300

    save only state

--- utils.py
+++ utils.py
@@ -4,14 +4,9 @@ import inspect
 
 
 def save_model(model, params, prefix, q, directory):
-    with open(f'{directory}/{prefix}_{q}.pkl', 'wb') as f:
-        pickle.dump(model, f)
-    with open(f'{directory}/{prefix}_{q}_cpu.pkl', 'wb') as f:
-        pickle.dump(model.cpu(), f)
     with open(f'{directory}/{prefix}_params_{q}.pkl', 'wb') as f:
         pickle.dump(params, f)
     torch.save(model.state_dict(), f'{directory}/{prefix}_{q}_state.pt')
-    torch.save(model, f'{directory}/{prefix}_{q}.pt')
 
 
 def load_model(model_class, prefix, q, directory):
