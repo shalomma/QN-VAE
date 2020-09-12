21e90c793bc2f4d40e28dc0beb77bed06e392fca
Maayan Shalom <sha.maayan@gmail.com>
2020-09-12 16:57:40 +0300

    sample

--- sample.py
+++ sample.py
@@ -2,7 +2,6 @@ import torch
 import argparse
 
 from utils import load_model, save_samples
-from qnvae import QNVAE
 from pixelcnn import PixelCNN
 
 
@@ -14,12 +13,6 @@ if __name__ == '__main__':
     directory = f'models/{args.timestamp}'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    quant_noise_probs = [0.25, 0.5, 0.75, 1]
-    for q_ in quant_noise_probs:
-        print(f'Train q={q_}')
-        model_pixelcnn, _ = load_model(PixelCNN, 'pixelcnn', q_, directory)
-        encoding = model_pixelcnn.sample((1, 8, 8), 64, label=None, device=device)
-        save_samples(encoding, directory, f'encoding_{q_}.png')
-        model_qnvae, _ = load_model(QNVAE, 'qnvae', q_, directory)
-        decoded = model_qnvae.decode_samples(encoding)
-        save_samples(decoded, directory, f'decoded_{q_}.png')
+    model_pixelcnn, _ = load_model(PixelCNN, 'pixelcnn', q=0, directory=directory)
+    encoding = model_pixelcnn.sample((1, 8, 8), 64, label=None, device=device)
+    save_samples(encoding, directory, f'encoding.png')
