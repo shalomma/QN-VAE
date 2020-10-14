0b7f8a145a2a4efa616ec9f4d9d336332f1704b7
Maayan Shalom <sha.maayan@gmail.com>
2020-10-14 19:34:05 +0300

    self.data.unsqueeze(1)

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ class Encoded(data.Dataset):
     def __init__(self, root_dir, q, indices, transform=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.data = torch.load(os.path.join(root_dir, f'encoded_data_{q}.pt'), map_location=device)
-        self.data = self.data.view(-1, 1, 8, 8).cpu().numpy()
+        self.data = self.data.unsqueeze(1).cpu().numpy()
         self.y = torch.load(os.path.join(root_dir, f'encoded_labels_{q}.pt'), map_location=device)
         self.y = self.y[indices].to(device).long()
         self.data = self.data[indices]
