1a4cae146692b203b82e4cbf30aaf608bc91a1ba
Maayan Shalom <sha.maayan@gmail.com>
2020-10-19 00:31:27 +0300

    directory_prior

--- sample.py
+++ sample.py
@@ -11,18 +11,19 @@ from pixelcnn import PixelCNN
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('timestamp', type=str, help='models timestamp')
+    parser.add_argument('qnvae_timestamp', type=str, help='vq-vae model timestamp')
+    parser.add_argument('prior_timestamp', type=str, help='models timestamp')
     parser.add_argument('--n_samples', type=int, default=64)
     args = parser.parse_args()
 
-    directory = f'models/{args.timestamp}'
+    directory = f'models/{args.qnvae_timestamp}/'
+    directory_prior = f'models/{args.qnvae_timestamp}/{args.prior_timestamp}'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    load_dir = f'models/{args.timestamp}'
-    quant_noise_probs = sorted([float(q.split('/')[-1][6:-3]) for q in glob.glob(f'{load_dir}/qnvae*.pt')])
+    quant_noise_probs = sorted([float(q.split('/')[-1][6:-3]) for q in glob.glob(f'{directory}/qnvae*.pt')])
     for q_ in quant_noise_probs:
         print(f'Sample q={q_}')
-        model_pixelcnn, params = load_model(PixelCNN, 'pixelcnn', q_, directory)
+        model_pixelcnn, params = load_model(PixelCNN, 'pixelcnn', q_, directory_prior)
         encoding = model_pixelcnn.sample((1, 7, 7), args.n_samples, label=None, device=device)
         save_samples(encoding, directory, f'latent_{q_}.png')
         encoding = (encoding * (params['levels'] - 1)).long()
@@ -30,4 +31,4 @@ if __name__ == '__main__':
         decoded = model_qnvae.decode_samples(encoding) + 0.5
         os.makedirs(f'{directory}/{q_}')
         for i in range(args.n_samples):
-            save_image(decoded[i], os.path.join(f'{directory}/{q_}', f'{i}.png'))
+            save_image(decoded[i], os.path.join(f'{directory_prior}/{q_}', f'{i}.png'))
