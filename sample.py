06be5f2f228973f17b0e741391d78c1191bb2289
Maayan Shalom <sha.maayan@gmail.com>
2020-10-19 00:06:10 +0300

    params['levels'] - 1

--- sample.py
+++ sample.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
 
     quant_noise_probs = [0.25, 0.5, 0.75, 1]
     for q_ in quant_noise_probs:
-        print(f'Train q={q_}')
+        print(f'Sample q={q_}')
         model_pixelcnn, params = load_model(PixelCNN, 'pixelcnn', q_, directory)
-        encoding = model_pixelcnn.sample((1, 8, 8), 4, label=None, device=device)
+        encoding = model_pixelcnn.sample((1, 7, 7), 4, label=None, device=device)
         save_samples(encoding, directory, f'latent_{q_}.png')
-        encoding = (encoding * params['levels']).long()
+        encoding = (encoding * (params['levels'] - 1)).long()
         model_qnvae, _ = load_model(QNVAE, 'qnvae', q_, directory)
         decoded = model_qnvae.decode_samples(encoding) + 0.5
         save_samples(decoded, directory, f'decoded_{q_}.png')
