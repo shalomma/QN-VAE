2dc54d28e9b2abb1c2e352e58f718a8041ae76cd
Maayan Shalom <sha.maayan@gmail.com>
2020-10-24 22:52:17 +0300

    temp gan dirs

--- gan.py
+++ gan.py
@@ -80,14 +80,15 @@ class Discriminator(nn.Module, ABC):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('timestamp', type=str, help='models timestamp')
     parser.add_argument('--epochs', type=int, help='epochs', default=100)
     args = parser.parse_args()
 
-    load_dir = f'./models/{args.timestamp}'
+    timestamp = str(datetime.now())[:-7].replace('-', '_').replace(' ', '_').replace(':', '_')
+    save_dir = f'./models/{timestamp}'
+    os.makedirs(save_dir, exist_ok=True)
     os.makedirs("images", exist_ok=True)
 
-    logging.config.fileConfig('logging.ini', defaults={'logfile': f'{load_dir}/training_prior.log'},
+    logging.config.fileConfig('logging.ini', defaults={'logfile': f'{save_dir}/training_prior.log'},
                               disable_existing_loggers=False)
     log = logging.getLogger(__name__)
 
@@ -140,10 +141,5 @@ if __name__ == '__main__':
     trainer.epochs = params['epochs']
     trainer.latent_dim = params['latent_dim']
     trainer.run()
-
-    timestamp = str(datetime.now())[:-7].replace('-', '_').replace(' ', '_').replace(':', '_')
-    save_dir = f'{load_dir}/{timestamp}'
-    os.makedirs(save_dir, exist_ok=True)
-
     save_model(model['generator'], params, 'generator', 'mnist', save_dir)
     save_model(model['discriminator'], params, 'discriminator', 'mnist', save_dir)
