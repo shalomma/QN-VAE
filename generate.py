9690bfedd5c921438ee0d64342446b8e0ea06406
Maayan Shalom <sha.maayan@gmail.com>
2020-10-24 22:32:35 +0300

    drop /

--- generate.py
+++ generate.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     directory = f'models/{args.gan_dir}/'
-    directory_img = f'{directory}/images/'
+    directory_img = f'{directory}/images'
     os.makedirs(directory_img, exist_ok=True)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
