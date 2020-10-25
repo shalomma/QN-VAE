017a4d16533eca1f11774de38f0e002f8a2cba5b
Maayan Shalom <sha.maayan@gmail.com>
2020-10-25 15:50:55 +0200

    dataset keep as tensor

--- dataset.py
+++ dataset.py
@@ -7,13 +7,13 @@ class Encoded(data.Dataset):
     def __init__(self, root_dir, q, indices, transform=None):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.data = torch.load(os.path.join(root_dir, f'encoded_data_{q}.pt'), map_location=device)
-        self.data = self.data.unsqueeze(1).cpu().numpy()
+        self.data = self.data.unsqueeze(1).type(torch.float32)  # .cpu().numpy()
         self.y = torch.load(os.path.join(root_dir, f'encoded_labels_{q}.pt'), map_location=device)
         self.y = self.y[indices].to(device).long()
         self.data = self.data[indices]
         if transform is not None:
             self.data = transform(self.data)
-        self.data = torch.tensor(self.data).to(device)
+        # self.data = torch.tensor(self.data).to(device)
 
     def __len__(self):
         return self.data.shape[0]
